59789a36064456a4b11f47786e1a751d90a09fc6
Andres <yancea@uwplatt.edu>
2022-11-22 11:56:58 -0600

    class for info and image of character.

---
+++ src/main/java/com/example/javafxtrytwo/CharacterProperties.java
@@ -0,0 +1,10 @@
+package com.example.javafxtrytwo;
+
+import java.util.List;
+
+public class CharacterProperties {
+    List <image>imageStates = new List<image>();
+    static String info; // hero name, size, powers
+
+
+}
