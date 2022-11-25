c19052ded22ab9968d561ce1d59f34edc534f9bf
ElijahF <friedriche@uwplatt.edu>
2022-11-25 14:41:59 -0600

    @friedriche
    Updated JavaFX
    added buttons + texts for Main Menu
    Not a lot done but increased understanding of JavaFX

--- src/main/java/com/example/javafxtrytwo/HelloApplication.java
+++ src/main/java/com/example/javafxtrytwo/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.javafxtrytwo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,12 +10,27 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
+//    public void start(Stage stage) throws IOException {//
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml")); // creating controller?
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240); // setting scene to be the fxmlLoader
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
+//    Going with the "Parent" route instead of the "fxmlLoader" because all the docs/guides are for this parent root
+//    stage << scene << root node << node
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            Scene main_menu = new Scene(root, 420, 469); // can set initial size here
+            stage.setTitle("FIGHT GAME!"); // name?
+            stage.setScene(main_menu); // Setting stage to show screen
+            stage.show(); // Render the stage.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -27,6 +43,10 @@ public class HelloApplication extends Application {
         hero.print_name();
         System.out.println("console test text in main");
 
+
+//        pretty sure we need to do all our constructions BEFORE we launch the app otherwise,
+//        at least for testing purposes otherwise it won't reach the lines of code after launch.
+//        You would need to have an activation setup within the app already
         launch();
 
     }
--- src/main/java/com/example/javafxtrytwo/HelloController.java
+++ src/main/java/com/example/javafxtrytwo/HelloController.java
@@ -5,12 +5,42 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    public Button button1;
+    public Button button_start;
+    public Button button_shop;
+    public Button button_options;
+    public Button button_exit;
     @FXML
-    private Label welcomeText;
+    private Label startButtonText;
+    @FXML
+    private Label shopButtonText;
+    @FXML
+    private Label optionsButtonText;
+
+//    dont think we need exitbut text, just need to call an exit method
+    @FXML
+    private Label exitButtonText;
+
+
+    // so here we'd want more buttons that on click will do a transition into a different scene(node)?
+    @FXML
+    protected void startButtonClick() {
+        startButtonText.setText("Start is not yet implemented!");
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void shopButtonClick() {
+        shopButtonText.setText("Shop is not yet implemented!");
     }
+    @FXML
+    protected void optionsButtonClick() {
+        optionsButtonText.setText("Options are not yet implemented!");
+    }
+
+
+    //    exit just needs to exit, not change any text
+    @FXML
+    protected void exitButtonClick() {
+        exitButtonText.setText("no exit yet >:(");
+    }
+
 }
