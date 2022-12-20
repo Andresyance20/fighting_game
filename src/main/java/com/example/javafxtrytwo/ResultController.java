dc6af0f35833ec6e3ecd59499e8ddd88c83a8339
Elijah Friedrich <friedriche@uwplatt.edu>
2022-12-20 20:19:19 +0000

    Revert "update Results"
    This reverts commit bd3ee2d9e3948e2f3c4f32d29116fb0d5f295781

--- src/main/java/com/example/javafxtrytwo/ResultController.java
+++ src/main/java/com/example/javafxtrytwo/ResultController.java
@@ -3,103 +3,34 @@ package com.example.javafxtrytwo;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class ResultController {
 
     private Scene mainScene;
     private Scene fightScene;
+
     private Hero playerHero1;
     private Hero playerHero2;
     private Hero playerHero3;
-    private Hero playerActiveHero;
-    private Hero heroAI;
-    private int money;
-    private int hpPotionCount;
-    private int attackPotionCount;
-    private int superPotionCount;
 
-    public void setToMainMenu(Scene scene, Hero hero1, Hero hero2, Hero hero3, Hero activeHero, Hero heroai) {
+    public void setToMainMenu(Scene scene, Hero hero1, Hero hero2, Hero hero3) {
         mainScene = scene;
         playerHero1 = hero1;
         playerHero2 = hero2;
         playerHero3 = hero3;
-        playerActiveHero = activeHero;
-        heroAI = heroai;
+
     }
-
-    public void setToFightScene(Scene scene, Hero hero1, Hero hero2, Hero hero3, Hero activeHero, Hero heroai) {
-
+    public void setToFightScene(Scene scene, Hero hero1, Hero hero2, Hero hero3)
+    {
         fightScene = scene;
         playerHero1 = hero1;
         playerHero2 = hero2;
         playerHero3 = hero3;
-        playerActiveHero = activeHero;
-        heroAI = heroai;
     }
-
-    public void openMainMenu(ActionEvent actionEvent)
+   /* public void openMainMenu(ActionEvent actionEvent)
     {
-
-        playerHero1 = playerHero1;
-        playerHero2 = playerHero2;
-        playerHero3 = playerHero3;
-        heroAI = heroAI;
-
-        // add some logic to make sure there is an active hero before going to fightscene?
-        if (playerHero1.getActive() == true)
-        {
-            System.out.println("Hero1 is: " + playerHero1.getActive());
-            playerActiveHero = playerHero1;
-        }
-        if (playerHero2.getActive() == true)
-        {
-            System.out.println("Hero2 is: " + playerHero2.getActive());
-            playerActiveHero = playerHero2;
-        }
-        if (playerHero3.getActive() == true)
-        {
-            System.out.println("Hero3 is: " + playerHero3.getActive());
-            playerActiveHero = playerHero3;
-            System.out.println(playerActiveHero.getCurrenthp());
-        }
-
-
         Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         primaryStage.setScene(mainScene);
-    }
-
-    public void openFightScene(ActionEvent actionEvent) {
-
-        playerHero1 = playerHero1;
-        playerHero2 = playerHero2;
-        playerHero3 = playerHero3;
-        heroAI = heroAI;
-
-        // add some logic to make sure there is an active hero before going to fightscene?
-        if (playerHero1.getActive() == true)
-        {
-            System.out.println("Hero1 is: " + playerHero1.getActive());
-            playerActiveHero = playerHero1;
-        }
-        if (playerHero2.getActive() == true)
-        {
-            System.out.println("Hero2 is: " + playerHero2.getActive());
-            playerActiveHero = playerHero2;
-        }
-        if (playerHero3.getActive() == true)
-        {
-            System.out.println("Hero3 is: " + playerHero3.getActive());
-            playerActiveHero = playerHero3;
-            System.out.println(playerActiveHero.getCurrenthp());
-        }
-
-
-        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        primaryStage.setScene(fightScene);
-    }
-
-    public Button a ;
-    public Button  b;
+    }*/
 }
